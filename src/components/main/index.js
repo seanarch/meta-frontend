c746833a515a2dfc4dfd83f0f0705d527bf0f7bc
seanarch <zhuqingyun@outlook.com>
2023-12-09 13:34:39 -0700

    main banner section with style

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,10 +1,25 @@
+import food from "../../assets/restauranfood.jpg";
 import "./style.css";
 
 function Main() {
   return (
     <main>
-      <div className="banner"></div>
-      Main
+      <div className="banner">
+        <div className="content">
+          <div className="text-content">
+            <h1 className="title">Little Lemon</h1>
+            <h2 className="subtitle">Chicago</h2>
+            <p>
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <button>Reserve a Table</button>
+          </div>
+          <div className="image-content">
+            <img src={food} alt="food" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
