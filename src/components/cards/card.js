9799cca62b87349ca9bf05b164083d835b5736fc
seanarch <zhuqingyun@outlook.com>
2023-12-09 21:09:41 -0700

    Add cards and adjust style

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,7 +1,22 @@
-function Card() {
+import "./style.css";
+
+function Card({ image, title, price, desc }) {
   return (
-    <div>
-      <h1>Card</h1>
+    <div className="single-card">
+      <div class="card">
+        <img className="card-image" src={image} alt="Card image cap" />
+        <div className="card-body">
+          <div className="card-title-price">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-price">{price}</p>
+          </div>
+
+          <p className="card-text">{desc}</p>
+          <a href="#" id="orderdelivery">
+            Order a delivery
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,12 +1,30 @@
 import Card from "./card";
 import "./style.css";
+import greekSalad from "../../assets/greeksalad.jpg";
+import bruschetta from "../../assets/bruschetta.jpg";
+import lemonDessert from "../../assets/lemondessert.jpg";
 
 function Cards() {
   return (
     <div className="card-container">
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        image={greekSalad}
+        price="$12.99"
+        title="Greek salad"
+        desc="The famous greek salad of crispy lettuce, tomatoes, cucumbers, onions, olives, and feta cheese."
+      />
+      <Card
+        image={bruschetta}
+        price="$5.99"
+        title="Bruschetta"
+        desc="Our Bruschetta is made from grilled bread that has been xx with garlic and seasoned with salt and olive oil."
+      />
+      <Card
+        image={lemonDessert}
+        price="$4.99"
+        title="Lemon Dessert"
+        desc="This comes straight from grandma's receipe book, sourced and is as authentic as can be imaged."
+      />
     </div>
   );
 }
